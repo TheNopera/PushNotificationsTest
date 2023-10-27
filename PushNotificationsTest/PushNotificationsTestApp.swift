3a89dea8b8e76991abd63f4479e7eef15d1a8654
Daniel Ishida <ishidadecol@gmail.com>
2023-10-27 17:19:20 -0300

    Initial commit

---
+++ PushNotificationsTest/PushNotificationsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsTestApp.swift
+//  PushNotificationsTest
+//
+//  Created by Daniel Ishida on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
